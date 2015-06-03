37427d4fef4bea44d301a968cece7479a1482bc1
Victor Kohler <victor@houseofradon.com>
2015-06-03 23:49:41 +0200

    initial commit

---
+++ builtin.h
@@ -0,0 +1,87 @@
+
+/*
+Function declaration for built in commands
+*/
+int shv_cd(char **args);
+int shv_help(char **args);
+int shv_exit(char **args);
+int shv_path(char **args);
+
+/*
+List of builtin commands, followed by their corresponding functions.
+*/
+
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit",
+    "path"
+};
+
+int (*builtin_func[]) (char **) = {
+    &shv_cd,
+    &shv_help,
+    &shv_exit,
+    &shv_path
+};
+
+int shv_num_builtins()
+{
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+int shv_cd(char **args)
+{
+    if(args[1] == NULL){
+        fprintf(stderr, "shv: expected argument to \"cd\"\n");    
+    }else{
+        if(chdir(args[1]) != 0){
+            perror("shv");    
+        }
+    }
+    return 1;
+}
+
+int shv_help(char **args)
+{
+    int i;
+    printf("The SHiV Shell version 0.0.1\n");
+    printf("Type program names and arguments and hit ender\n");
+    printf("The following are built in:\n");
+    
+    for (i = 0; i < shv_num_builtins(); i++){
+        printf("  %s\n", builtin_str[i]);    
+    }
+
+    printf("Use the man command for info on other programms.\n");
+    return 1;
+}
+
+
+int shv_path(char **args)
+{
+    char cwd[1024];
+
+    if(!cwd){
+        fprintf(stderr, "Could not allocate cwd\n");
+    }
+
+    if(getcwd(cwd, sizeof(cwd)) != NULL){
+        printf("%s\n", cwd);
+    }else{
+        perror("Error. Could not get the current path");    
+    }
+
+    return 1;
+
+
+}
+
+
+int shv_exit(char **args)
+{
+    write_history("shv_history");
+    return 0;    
+}
+
+
---
+++ shv.c
@@ -0,0 +1,200 @@
+
+/************************************
+
+    [THE SHiV SHELL]
+
+    * Author: Victor Köhler
+    * Version: 0.0.1
+
+   * Compilation: gcc -o shv shv.c
+   * With readline: -lreadline
+   * With ncurses: -lncurses
+
+*************************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include"builtin.h"
+
+#define SHV_VERSION "0.0.1"
+#define SHV_RL_BUFSIZE 1024
+#define SHV_TOK_BUFSIZE 64
+#define SHV_TOK_DELIM " \t\r\n\a"
+
+
+/*
+SHiV Core function declarations
+*/
+void shv_loop(void);
+char *shv_read_line(void);
+char **shv_split_line(char *line);
+int shv_launch(char **args);
+int shv_execute(char **args);
+
+/*
+SHiV core functions
+*/
+int main(int argc, char *argv)
+{
+    printf("\x1b[31mSHiV Shell version %s. Type \"help\" for more.\x1b[0m\n", SHV_VERSION); 
+    shv_loop();
+    return EXIT_SUCCESS;
+}
+
+
+void shv_loop(void)
+{
+    char *line;
+    char **args;
+    int status;
+
+    read_history("shv_history");
+
+    do{
+        line = shv_read_line();
+        args = shv_split_line(line);
+        status = shv_execute(args);
+
+        free(line);    
+        free(args);
+    } while (status);
+
+
+}
+
+
+char *shv_read_line(void)
+{
+
+    int bufsize = SHV_RL_BUFSIZE;
+    char *buffer = malloc(sizeof(char) * bufsize);
+
+    if (!buffer){
+        fprintf(stderr, "shv: memory allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((buffer = readline("[shv]-$ ")) != NULL){
+        if(buffer[0] != 0){
+            add_history(buffer);
+            return buffer;
+        }
+    }
+
+    free(buffer);
+
+    /* Manual implementation of reading line to buffer
+    *************************************************
+    int c;
+    int position = 0;
+
+    while (1){
+        //Read a character
+        c = getchar();
+
+        if (c == EOF || c == '\n'){
+            buffer[position] = '\0';
+            return buffer;    
+        }else{
+            buffer[position] = c;
+        }
+        position ++;
+
+        //If we exceed buffer, reallocate!
+        if (position >= bufsize){
+            bufsize += SHV_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer){
+                fprintf(stderr, "shv: memory reallocation error\n");
+                exit(EXIT_FAILURE);    
+            }    
+        }
+    }*/
+}
+
+
+char **shv_split_line(char *line)
+{
+    int bufsize = SHV_TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+
+    if(!tokens){
+        fprintf(stderr, "shv: memory allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, SHV_TOK_DELIM);
+
+    while(token != NULL){
+        tokens[position] = token;
+        position ++;    
+
+        if(position >= bufsize){
+            bufsize += SHV_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+            
+            if(!tokens){
+                fprintf(stderr, "shv: memory reallocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok(NULL, SHV_TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
+
+
+int shv_launch(char **args)
+{
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    
+    if (pid == 0){
+        //child process
+        if(execvp(args[0], args) == -1){
+            perror("shv");    
+        }
+        exit(EXIT_FAILURE);
+    }else if(pid < 0){
+        //Error forking
+        perror("shv");
+    }else{
+        //parent process
+        do{
+            wpid = waitpid(pid, &status, WUNTRACED);
+        }while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return 1;
+}
+
+
+
+int shv_execute(char **args)
+{
+    int i;
+    if (args[0] == NULL){
+        //empty command
+        return 1;
+    }
+
+    for (i = 0; i < shv_num_builtins(); i++){
+        if (strcmp(args[0], builtin_str[i]) == 0){
+            return (*builtin_func[i])(args);   
+        }
+    }
+    return shv_launch(args);
+}
+
+
+
+
